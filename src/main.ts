b0eefc8f27f141d32b0cd42977b68de14e52a875
Ruan Rickelme Lucena Ramos <ruanlucenaramos@gmail.com>
2025-09-19 19:06:58 -0300

    updating routes.js to ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import {createApp} from 'vue'
 import App from "./App.vue"
 import './style.css' // importa o// Tailwind
-import router from './route/routes.js'
+import router from './route/routes'
 
 createApp(App).use(router).mount('#app');
